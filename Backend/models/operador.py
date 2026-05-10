1f0d22a35aeac4aa4fdf321bc96f43c1b5213b0e
Bia-z <beatriz-cmt@hotmail.com>
2026-05-10 20:36:50 -0300

    apagandoa pasta testes

--- Backend/models/operador.py
+++ Backend/models/operador.py
@@ -1,4 +1,4 @@
-#define o que é um operador, criadno uma formula, todo operador tem id, nome e email, no caso
+"""define o que é um operador, criadno uma formula, todo operador tem id, nome e email, no caso"""
 
 class Operador:
     def __init__(self, id=None, nome="", email=""):
--- Backend/test.py
+++
@@ -1,6 +0,0 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-senha = "123"
-hash_gerado = generate_password_hash(senha)
-print("Hash:", hash_gerado)
-print("Confere:", check_password_hash(hash_gerado, senha))
